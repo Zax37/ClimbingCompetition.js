8030485f396864b252d5250429c4d77dec472220
Zax37 <c37zax@gmail.com>
2018-04-01 04:56:21 +0200

    Fix main module - separate server from application.

---
+++ src/application.js
@@ -0,0 +1,26 @@
+/**
+ * Server description - requirements, middleware, request listeners, etc.
+ */
+// LOAD APPLICATION PROPERTIES
+const properties = require('./properties');
+
+// EXPRESS FRAMEWORK
+const express = require('express');
+const application = express();
+
+// MAPPINGS - API - BEFORE SSL FORCING
+application.use('/api/v1/', require('./api/v1'));
+
+// SECURITY - FORCE SSL FOR WEB MAPPINGS
+const forceSSL = require('express-force-ssl');
+application.use(forceSSL);
+application.set('forceSSLOptions', {
+    httpsPort: properties.httpsPort
+});
+
+// MAPPINGS - STATIC RESOURCES
+application.use(express.static('public'));
+// MAPPINGS - WEB APP
+application.use(require('./webpage'));
+
+module.exports = application;
